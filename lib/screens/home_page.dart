2d9cc93fe5a2818e29691ad68d6418d1420bf984
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-01-27 10:40:55 +0530

    app bar color cahnge

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFFFFFBFE),
         title: Text(
           'Unnamed Road',
           style: TextStyle(
