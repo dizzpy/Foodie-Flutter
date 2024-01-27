54cc5aceb7b02fae5c165ad1e9a2a86a55427994
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-01-27 12:40:36 +0530

    prod page app bar done

--- lib/screens/productItem.dart
+++ lib/screens/productItem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodie/const/color.dart';
 
 class ProductItem extends StatefulWidget {
   const ProductItem({super.key});
@@ -10,8 +11,54 @@ class ProductItem extends StatefulWidget {
 class _ProductItemState extends State<ProductItem> {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Color(0xFFFF0000),
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            print('Home Button Pressed');
+          },
+          child: Container(
+            height: 30.0,
+            width: 30.0,
+            margin: EdgeInsets.all(7.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100.0),
+              border: Border.all(
+                width: 1.5,
+                color: colorSecondaryBlack,
+              ),
+            ),
+            child: Icon(
+              Icons.arrow_back,
+              color: colorSecondaryBlack,
+            ),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              print('Home Button Pressed');
+            },
+            child: Container(
+              padding: EdgeInsets.all(5),
+              height: 42.0,
+              width: 42.0,
+              margin: EdgeInsets.all(7.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100.0),
+                border: Border.all(
+                  width: 1.5,
+                  color: colorSecondaryBlack,
+                ),
+              ),
+              child: Icon(
+                Icons.favorite,
+                color: colorMainRed,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
