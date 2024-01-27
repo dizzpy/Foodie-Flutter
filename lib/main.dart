e8c364f0d6a1018e611ba1f1fc667f7ad8946271
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-01-27 12:24:02 +0530

    created product item page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
+import 'screens/productItem.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: ProductItem(),
     );
   }
 }
---
+++ lib/screens/productItem.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatefulWidget {
+  const ProductItem({super.key});
+
+  @override
+  State<ProductItem> createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Color(0xFFFF0000),
+    );
+  }
+}
